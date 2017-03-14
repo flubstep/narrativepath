a298c92540d08b25f35459245d027225484ed74d
Albert Sheu <albert.sheu@gmail.com>
2017-03-13 22:40:37 -0700

    Update scripts for Heroku

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
+const db = require('./models/index');
+
 const index = require('./routes/index');
 const users = require('./routes/users');
 
